5fdf08482f139c29b89f9c04facb85a3f91e4834
mdirgha18 <dirgha.mankad702@gmail.com>
2025-12-23 11:11:49 +0530

    Time: 256 ms (76.47%), Space: 43.1 MB (82.35%) - LeetHub

---
+++ 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def maxTwoEvents(self, events):
+        """
+        :type events: List[List[int]]
+        :rtype: int
+        """
+        events.sort()
+        pq = []
+        max_val = 0
+        ans = 0
+
+        for start, end, value in events:
+            while pq and pq[0][0] < start:
+                max_val = max(max_val, heapq.heappop(pq)[1])
+            ans = max(ans, max_val + value)
+            heapq.heappush(pq, (end, value))
+        return ans
+        
