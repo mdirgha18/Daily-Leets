d9c879b86acd11dace70b1c438b154b6cb59fa36
mdirgha18 <dirgha.mankad702@gmail.com>
2026-01-24 15:15:16 +0530

    Time: 1086 ms (40%), Space: 24.8 MB (70.67%) - LeetHub

---
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def minPairSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # Step 1: Sort the array in ascending order
+        nums.sort()
+
+        # Step 2: Initialize pointeres at the start and end of the sorted array
+        left, right = 0, len(nums)  - 1 
+
+        # Step 3: Initialize a variable to store the minimum of the maximum pair sum 
+        min_max_pair_sum = float('-inf')
+
+        # Step 4: Iterate until the pointers meet 
+        while left < right:
+            # Step 5: Calculate the current pair sum 
+            current_pair_sum = nums[left] + nums[right]
+
+            # Step 6: Update the minimum of maximum pair sum 
+            min_max_pair_sum = max(min_max_pair_sum, current_pair_sum)
+
+            # Step 7: Move the pointers towards the center
+            left += 1
+            right -= 1
+
+        # Step 8: Return the minimum of the maximum pair sum
+        return min_max_pair_sum
